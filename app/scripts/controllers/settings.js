fdbb16791f981003c6961334ad6372ab3edc47b2
Janne Laukkanen <janne.laukkanen83@gmail.com>
2014-07-09 02:25:25 +0300

    add feature password change

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -10,11 +10,33 @@
 angular.module('greenhouseApp')
   .controller('SettingsCtrl', ['$scope', '$stateParams', 'user', function ($scope, $stateParams, user) {
 
-    console.log('settingsCTRL')   
-    
-
     $scope.changePassword = function () {
-        
+    		$scope.showAlert = false;
+    	  var oldPassword = $scope.fields.old
+    	    , newPassword = $scope.fields.new_confirm
+
+    	  if ($scope.fields.new_password === $scope.fields.new_confirm) {
+	        
+	        user.auth.changePassword($scope.user.email, oldPassword, newPassword, function (error, success) {
+	        	if (!error) {
+						  $scope.$apply(function () {
+						    $scope.showAlert = true;
+						    $scope.alertClass = "success";
+						    $scope.alertMessage = "Password changed successfully.";
+					    });
+					  } else {
+					  	$scope.$apply(function () {
+						  	$scope.showAlert= true;
+						  	$scope.alertClass = "danger";
+						  	$scope.alertMessage = "Password change failed.";
+					    });
+					  }
+					});
+	      } else {
+	      	$scope.showAlert= true;
+			  	$scope.alertClass = "danger";
+			  	$scope.alertMessage = "Given new passwords are not equal!";
+	      }
     }
   }
 ]);
